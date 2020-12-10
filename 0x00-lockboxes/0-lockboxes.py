fafa4bf90849e7bb517d759d22d9a4ca50eeafd4
zelda096 <miguelangel697@gmail.com>
2020-12-10 09:37:47 -0500

    lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+ 
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+
+    key = 0
+    KeyC = [0]
+    if len(boxes) == 0:
+        return(True)
+    for key in KeyC:
+        for key in boxes[key]:
+            # print("estos es boxes key",boxes[llave])
+            # print("estos es keys",key)
+            if key in KeyC:
+                # llavero.append(key)
+                # print(KeyC)
+                continue
+            else:
+                if key <= len(boxes) - 1:
+                    KeyC.append(key)
+                else:
+                    continue
+                # key = keys
+                # a = keys
+                # break
+    if len(KeyC) == len(boxes):
+        return(True)
+    else:
+        return(False)
